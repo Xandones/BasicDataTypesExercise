11724c4a2fd5d91d4c84b3a9cd954fc1027e1957
Alexandro H. Lopes <karkekoisaaeh@outlook.com>
2022-09-27 17:32:41 -0300

    First commit

---
+++ app/src/main/java/com/alexandrohlopes/basicdatatypesexercise/Exercise.kt
@@ -0,0 +1,20 @@
+package com.alexandrohlopes.basicdatatypesexercise
+
+fun main() {
+    val str = "Android Masterclass"
+    val float = 13.37f
+    val double = 3.14159265358979
+    val short : Short = 25
+    val int = 2020
+    val long : Long = 18881234567
+    val bool = true
+    val char = 'a'
+    println("String: $str")
+    println("Float: $float")
+    println("Double: $double")
+    println("Short: $short")
+    println("Integer (32 bits): $int")
+    println("Long: $long")
+    println("Boolean: $bool")
+    println("Char: $char")
+}
